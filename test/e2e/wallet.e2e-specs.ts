96b2c8772fe11403148e9c6ecb170d51f78f934d
Richard Le <levanthanh.ptit@gmail.com>
2022-11-30 15:32:45 +0700

    fix: update test

--- test/e2e/wallet.e2e-specs.ts
+++ test/e2e/wallet.e2e-specs.ts
@@ -1,7 +1,9 @@
+import { testHelper } from './test-entrypoint.e2e-spec';
 import { expect } from 'chai';
 
 describe('[dummy] private key management', () => {
   it('should generate private key successfully', async () => {
-    expect(true).to.equal(false);
+    const privateKey = testHelper.createSolanaKeyPair();
+    expect(!!privateKey.privateKey).to.be.true;
   });
 });
