d1668aab5712580565fad26029eba4cd296a661b
0xTraderTrou <khangtran.btc@gmail.com>
2022-12-06 22:46:06 +0700

    feat: added whitelisted collections

--- src/swap/controllers/config.controller.ts
+++ src/swap/controllers/config.controller.ts
@@ -33,6 +33,69 @@ export class SwapConfigController {
           type: 'nft-collection',
           name: 'DeGods',
         },
+        {
+          id: 'b87cf16fde5ea9117831766cd0ca86f7ec7cc86aa78199ec26a81d5e37dee369',
+          image:
+            'https://nftstorage.link/ipfs/bafybeidklga2k23b7zxtlna5rcp2casiktfndvsxynimucwiiyvcgpqq54/84.png',
+          type: 'nft-collection',
+          name: 'Just tiger',
+        },
+        {
+          id: '2e44513374fb2e104fae68c9253432a143f5f3ec6e3549fa8235d06d77e7cddc',
+          image:
+            'https://nftstorage.link/ipfs/bafybeifwgtllf23btjjxfg72rhyuejo46jffm2mdr3mep4faewxferyqka/536.png',
+          type: 'nft-collection',
+          name: 'Lurker',
+        },
+        {
+          id: '903c2d6bb7b35fc58e9df37a4367dcbbafb905d08bdfdf7394544105b8d83106',
+          image:
+            'https://nftstorage.link/ipfs/bafybeicvgxmua6v42tfek5llzw4vtqch43i4li3r7giqo7ebisndxytnuu/399.jpeg',
+          type: 'nft-collection',
+          name: 'AngryCats',
+        },
+        {
+          id: '47000db61c42e7613bf36f1fed177cbbb378b5ad61dfc4f06d1e80a8ad0aed9f',
+          image:
+            'https://nftstorage.link/ipfs/bafybeib7z3rbaodrg6l6zpypaamxxof3antmflllcfxt6vitovhh25ukyi/8690.png',
+          type: 'nft-collection',
+          name: 'Hasuki',
+        },
+        {
+          id: '951e97fc5b24026c3e6cab1de6810a5a9c1e936baa149e6e17a5d76a2dc8c273',
+          image:
+            'https://arweave.net/2CjjpF34uCI59Ab2jc017KMmqWKkZjZsLg8wLeOEb3s',
+          type: 'nft-collection',
+          name: 'Fatal Foxes',
+        },
+        {
+          id: 'c5491b3157ba13c779a0eca399572489d3cdd6daae6d5eb2d7ae1d599a012703',
+          image:
+            'https://nftstorage.link/ipfs/bafybeif7ydqzsfh44plj6jzl5emix6wkoviiri6ocydjk6lk3on65tdbhu/253.jpeg',
+          type: 'nft-collection',
+          name: 'Splatter Kids',
+        },
+        {
+          id: 'c3e7928c1fad8ecb945fa365e4a7e485fead5d9db5b0b7a7d41c52f427e62a35',
+          image:
+            'https://www.arweave.net/t-ZGgtMW2-CFEEKLCrCk6UONh4yDW5yZKChcoRFoxs0?ext=png',
+          type: 'nft-collection',
+          name: 'Skully Boys',
+        },
+        {
+          id: '8c7df16222c60acf07d026a392e61dd15829e5da9902d3ba53977520bc25aa0f',
+          image:
+            'https://arweave.net/Qzk6lNgzj_ylmjyvVWwE3mO_k-Q5nX9Uge2aC0MwDw4/2003.png',
+          type: 'nft-collection',
+          name: 'Degen Strays Club',
+        },
+        {
+          id: 'be8d3f2975099d695c3b3414fedd95f85436ccd84687c1bbc0cc9e1175c704ba',
+          image:
+            'https://bitmon-images.bitmon.io/8emdpbxVaDZeixfTScGQFgdfwSaZKqHxPNs3vwjmGXc.png',
+          type: 'nft-collection',
+          name: 'Bitmons',
+        },
       ],
     };
   }
