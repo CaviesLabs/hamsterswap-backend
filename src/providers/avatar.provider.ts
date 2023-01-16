0bd5d684801a4284f40eb180a5defc421948aad5
0xTraderTrou <khangtran.btc@gmail.com>
2023-01-16 23:43:19 +0700

    fix: fixed avatar path

--- src/providers/avatar.provider.ts
+++ src/providers/avatar.provider.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AvatarProvider {
-  private static baseUrl = 'https://avatars.hamsterbox.xyz/api';
+  private static baseUrl = 'https://avatars.hamsterbox.xyz/api/images';
 
   generateRandom(example = '1234'): string {
     return `${AvatarProvider.baseUrl}/beam/120/${example}`;
