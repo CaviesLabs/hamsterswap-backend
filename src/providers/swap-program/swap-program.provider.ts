2fcf7d1e49d30f792ba8fdd7dd3b032cb0f72130
0xTraderTrou <khangtran.btc@gmail.com>
2023-01-17 00:06:36 +0700

    feat: using paid rpc

--- src/providers/swap-program/swap-program.provider.ts
+++ src/providers/swap-program/swap-program.provider.ts
@@ -9,7 +9,7 @@ import { OCSwapPlatformRegistry, OCSwapProposal } from './swap.type';
 
 export const SOLANA_DEVNET_RPC_ENDPOINT = 'https://api.devnet.solana.com';
 export const SOLANA_MAINNET_RPC_RPC_ENDPOINT =
-  'https://api.mainnet-beta.solana.com';
+  'https://boldest-few-field.solana-mainnet.quiknode.pro/0ffa9f9f5e9141aa33a030081b78fdfe40bfbae6/';
 
 /**
  * @dev Swap Program Provider acts as an interface to interact with hamsterswap program on solana.
